ec161e1896a279e798e1566775006a6bcd668c20
Cam <Contillex@gmail.com>
2021-07-23 20:45:09 +0100

    [TASK] Begun Avatar Module Component

--- components/Modules/Sidebar/Sidebar.tsx
+++ components/Modules/Sidebar/Sidebar.tsx
@@ -1,14 +1,35 @@
+import Image from 'next/image'
+import { Avatar } from './Avatar'
+
 import s from './sidebar.module.scss';
 
 export function Sidebar() {
     return (
     <>
         <div className="flex flex-col w-64">
-            <div className="flex flex-col h-0 flex-1">
-                <div className={`${s.ares_sidebar} flex-1 flex flex-col pt-5 pb-4 overflow-y-auto`}>
-                    <div className="mt-5 flex-1 px-2 space-y-1">
-                        
+            <div className={`${s.ares_sidebar} container flex flex-col h-0 flex-1`}>
+                <div className="flex flex-col pt-5 pb-1 overflow-y-auto">
+                    <div className="pb-5 flex justify-center">
+                        <div className={`${s.ares_logo}`}>
+                            <Image
+                                src="/static/images/logo.png"
+                                width={104}
+                                height={59}
+                                alt="Ares Logo"
+                            />
+                        </div>
                     </div>
+
+                    <div className="pt-5 flex justify-center">
+                        <Avatar />
+                    </div>
+
+                    <div className="pt-5 flex justify-center">
+                        <div className="grid grid-cols-1">
+                            nav goes here
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
